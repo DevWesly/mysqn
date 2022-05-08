a045ac8ad18715918a2b4fa4658d9dd1ea980496
DevWesly <dev.lucaswesley@gmail.com>
2022-05-08 15:28:43 -0300

    Sequelize/Mysql configurados

--- database/config.js
+++ database/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-    "username": process.env.DBUSER,
+    "username":process.env.DBUSER,
     "password": process.env.DBPASS,
-    "database": process.env.DBNAME,
+    "database":process.env.DBNAME,
     "host": process.env.DBHOST,
     "dialect": process.env.DBDIALECT,
-    "port": process.env.DBPORT
+    "port": process.env.DBPORT,
 }
