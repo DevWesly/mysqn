b3338b2bfa7a2b05b11f9efc02361e2db88323e0
DevWesly <dev.lucaswesley@gmail.com>
2022-10-22 13:00:42 -0300

    configurando e estruturando o sequelize para o uso

--- database/config.js
+++ database/config.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 
-module.exports = {
-    "username": process.env.DBUSER,
-    "password": process.env.DBPASS,
-    "database": process.env.DBNAME,
-    "host": process.env.DBHOST,
-    "dialect": process.env.DBDIALECT,
-    "port": process.env.DBPORT
-}
+
+
+const config = {
+  username: process.env.DBUSER ,
+  password:process.env.DBPASS,
+  host:process.env.DBPORT,
+  port:process.env.DBPORT,
+  dialect:process.env.DBDIALECT,
+  database:process.env.DBNAME
+}; 
+
+module.exports = config;
