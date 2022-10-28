0cbcee96e76d7137c9d6bcee2b88dcad344212cf
DevWesly <dev.lucaswesley@gmail.com>
2022-10-28 14:10:08 -0300

    configurando os models publicacao e usuarios

---
+++ database/models/Publicacao.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, DataTypes) => {
+    let publicacao = sequelize.define(
+        "Publicacao", // Nome do model
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true
+            },
+
+            texto: {
+                type: DataTypes.STRING,
+            },
+            imagem: {
+                type: DataTypes.STRING
+            },
+            usuarios_id: {
+                type: DataTypes.INTEGER
+            }
+
+        }, {
+            tableName: "publicacoes",
+
+
+
+            timestamps: true,
+            paranoid: false
+
+
+
+        }
+    )
+
+    publicacao.associate = (models) => {
+        publicacao.belongsTo(models.Usuario, {
+            foreignKey: "usuarios_id",
+            as: "usuario_id"
+        });
+        publicacao.belongsToMany(models.Usuario,{
+            through:"curtidas",//<= nome da tabela de ligação
+            foreignKey:"publicacoes_id",//
+            otherKey:"usuarios_id",//coluna da table auxiliar que tem o id da model que esta se relacionando,
+            as:"curtidores",
+            timestamps: false
+        })
+
+    }
+
+
+    return publicacao
+}
---
+++ database/models/Usuario.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, DataTypes) => {
+
+    let usuario = sequelize.define(
+        "Usuario", {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true
+            },
+            nome: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            senha: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            foto: {
+                type: DataTypes.STRING,
+                allowNull: true
+            }
+        }, {
+            tableName: "usuarios",
+            timestamps: true,
+            paranoid: true
+        }
+    )
+    usuario.associate = (models) => {
+        usuario.hasMany(models.Publicacao, {
+            foreignKey: "usuarios_id",
+            as: "publicacoes"
+        })
+
+    }
+
+    return usuario;
+
+};
